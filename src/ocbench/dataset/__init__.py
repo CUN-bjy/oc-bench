d588e8cfb6d0eb6cfd8e29a5b193eb1e7afb3010
CUN-bjy <wnsdlqjtm@naver.com>
2023-05-17 14:38:09 +0900

    update multi-object-dataset and add ARI metric
    fix

--- src/ocbench/dataset/__init__.py
+++ src/ocbench/dataset/__init__.py
@@ -1,3 +1,4 @@
 from pathlib import Path
 
 DATASET_PATH = f"{Path(__file__).parent}/data/"
+from .multi_object_datasets import CaterWithMasks, ClevrWithMasks, MultiDSprites, ObjectsRoom, Tetrominoes
